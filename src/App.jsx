6924e694711a6597b69762012f5b8f07b22be795
FrentonGod <113124622+FrentonGod@users.noreply.github.com>
2023-03-28 21:04:50 -0600

    Inicializar el Proyecto - Lista de Hobbies

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+    const nombre = 'Darian Reyes Romero'
+  
+    return (
+        <div>
+          <h3>Lista de Hobbies {nombre}</h3>
+          <ol>
+            <li>Jugar videojuegos</li>
+            <li>Ver anime</li>
+            <li>Escuchar música</li>
+            <li>Pasar tiempo con mi novia</li>
+            <li>Investigar</li>
+            <li>Aprender a cocinar</li>
+            <li>Ver videos en youtube</li>
+            <li>Dormir</li>
+            <li>Programar</li>
+            <li>Leer libros</li>
+          </ol>
+      </div>
+    )
+  }
+  
+  export default App
